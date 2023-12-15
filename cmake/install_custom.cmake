2118fe1aaa71dcb250e7d88f45281ea38915db57
Dmitry Kazakov <dimula73@gmail.com>
2023-12-15 10:41:28 +0100

    Move custom python launcher into a separate directory

--- cmake/install_custom.cmake
+++ cmake/install_custom.cmake
@@ -4,4 +4,11 @@
 # ${CMAKE_COMMAND} -DSRC=bin/patch.exe -DDST=${EXTPREFIX} -P ${CMAKE_SOURCE_DIR}/cmake/install_custom.cmake
 #
 
-file(INSTALL ${SRC} DESTINATION ${DST})
+if (RENAME)
+    file(MAKE_DIRECTORY ${CMAKE_CURRENT_BINARY_DIR}/_krita-tmp/)
+    file(COPY_FILE ${SRC} ${CMAKE_CURRENT_BINARY_DIR}/_krita-tmp/${RENAME})
+    file(INSTALL ${CMAKE_CURRENT_BINARY_DIR}/_krita-tmp/${RENAME} DESTINATION ${DST})
+    file(REMOVE_RECURSE ${CMAKE_CURRENT_BINARY_DIR}/_krita-tmp/)
+else()
+    file(INSTALL ${SRC} DESTINATION ${DST})
+endif()
