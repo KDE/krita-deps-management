aa1841f0713f5fef4ccfffd13208b2a0045a3861
Dmitry Kazakov <dimula73@gmail.com>
2024-01-12 18:27:47 +0100

    Move env generation utilities into the common place in ci-utilities

--- tools/setup-env.py
+++ tools/setup-env.py
@@ -5,7 +5,7 @@ import argparse
 import sys
 import platform
 sys.path.append(os.path.join(os.path.dirname(__file__), '..', 'ci-utilities'))
-from components import CommonUtils
+from components import CommonUtils, EnvFileUtils
 
 # Capture our command line parameters
 parser = argparse.ArgumentParser(description='Utility to set up the environment for the split deps build')
@@ -67,76 +67,6 @@ if not sharedInstallDirectory is None:
 for var, value in environmentUpdate.items():
     print ('{} -> {}'.format(var, value))
 
-def getSaveResotreVarLine(var):
-    counter = 0
-    savedVariableName = '__OLD_{}'.format(var)
-    while True:
-        if not savedVariableName in os.environ:
-            break
-        counter = counter + 1
-        savedVariableName = '__OLD{}_{}'.format(counter, var)
-
-    if platform.system() == "Windows":
-        return ('set {saved}=%{var}%\n'.format(saved = savedVariableName, var = var),
-                'set {var}=%{saved}%\n'.format(saved = savedVariableName, var = var))
-    else:
-        return ('export {saved}=${var}\n'.format(saved = savedVariableName, var = var),
-                'export {var}=${saved}\n'.format(saved = savedVariableName, var = var))
-    
-def getVarSetterLine(var, value):
-    if platform.system() == "Windows":
-        return 'set {}={}\n'.format(var, value)
-    else:
-        return 'export {}={}\n'.format(var, value)
-
-def getVarUpdaterLine(var, values):
-    if not isinstance(values, list):
-        raise('The argument must be a list')
-    
-    if platform.system() == "Windows":
-        return 'set {var}={value};%{var}%\n'.format(var=var, value=';'.join(values))
-    else:
-        return 'export {var}={value}:${var}\n'.format(var=var, value=':'.join(values))
-
-def getScriptLine(script):
-    if platform.system() == "Windows":
-        return '{}\n'.format(os.path.abspath(script))
-    else:
-        return '. {}\n'.format(os.path.abspath(script))
-
-def writeEnvFile(directory, fileBaseName, environmentUpdate, environmentAppend = {}, extraActivationScripts = [], extraDeactivationScripts = []):
-    if platform.system() == "Windows" and fileBaseName.endswith('.bat'):
-        raise("Env file base name must not include the OS-specific suffix: {}".format(fileBaseName))
-    
-    fileSuffix = '.bat' if platform.system() == "Windows" else ''
-
-    varsToSave = list(environmentUpdate.keys())
-    if environmentAppend:
-        varsToSave.extend(environmentAppend.keys())
-
-    saveLines = []
-    restoreLines = []
-
-    for var in varsToSave:
-        save, restore = getSaveResotreVarLine(var)
-        saveLines.append(save)
-        restoreLines.append(restore)
-
-    with open(os.path.join(directory, fileBaseName + fileSuffix), 'w') as envFile:
-        envFile.writelines(saveLines)
-
-        for var, value in environmentUpdate.items():
-            envFile.write(getVarSetterLine(var, value))
-        for var, value in environmentAppend.items():
-            envFile.write(getVarUpdaterLine(var, value))
-        for script in extraActivationScripts:
-            envFile.write(os.path.abspath(script))
-
-    with open(os.path.join(directory, fileBaseName + '_deactivate' + fileSuffix), 'w') as envFile:
-        envFile.writelines(restoreLines)
-        for script in extraDeactivationScripts:
-            envFile.write(getScriptLine(script))
-
 activationScripts = []
 deactivationScripts = []
 
@@ -157,8 +87,8 @@ extraPathValues = [os.path.abspath(path) for path in arguments.path]
 for value in extraPathValues:
     print ('PATH += {}'.format(value))
 
-writeEnvFile(workingDirectory, arguments.output_file, 
-             environmentUpdate, 
-             extraActivationScripts = activationScripts, 
-             extraDeactivationScripts = deactivationScripts,
-             environmentAppend={'PATH': extraPathValues})
+EnvFileUtils.writeEnvFile(workingDirectory, arguments.output_file,
+            environmentUpdate,
+            extraActivationScripts = activationScripts,
+            extraDeactivationScripts = deactivationScripts,
+            environmentAppend={'PATH': extraPathValues})
