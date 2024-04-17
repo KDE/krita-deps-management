0cc8983ee4a33b8a99bc30688a89cf827c9897ca
Dmitry Kazakov <dimula73@gmail.com>
2024-04-17 15:49:38 +0200

    Change KDE_INSTALL_BUNDLEDIR to be relative
    Otherwise KDE's macosbundleqt script doesn't copy the
    krita.app directory properly.

--- tools/macos-toolchain-krita.cmake
+++ tools/macos-toolchain-krita.cmake
@@ -4,7 +4,7 @@ else()
     # todo
 endif()
 
-set(KDE_INSTALL_BUNDLEDIR "${CMAKE_INSTALL_PREFIX}/bin")
+set(KDE_INSTALL_BUNDLEDIR "bin")
 set(PYQT_SIP_DIR_OVERRIDE "${CMAKE_INSTALL_PREFIX}/share/sip/")
 set(PYTHON_INCLUDE_DIR "${CMAKE_INSTALL_PREFIX}/lib/Python.framework/Headers")
 set(MACOS_UNIVERSAL TRUE)
