b803e7852d34f792279c253e75a1823aee9f670a
Pedro Reis <pedroreis.ad@protonmail.com>
2025-04-28 23:11:52 +0100

    fix downloads dir for MacOS

--- tools/download-macos-tools.py
+++ tools/download-macos-tools.py
@@ -14,7 +14,8 @@ cmakeArchive = 'cmake-3.29.3-macos-universal.tar.gz'
 ninjaArchive = 'ninja-mac-1.12.1.zip'
 ccacheArchive = 'ccache-4.9.1-darwin.tar.gz'
 
-downloadsDir = os.environ.pop('EXTERNALS_DOWNLOAD_DIR')
+workingDirectory = os.getcwd()
+downloadsDir = os.path.join(workingDirectory, 'cache', 'downloads')
 
 # autocreate the downloads dialog, otherwise downloading will fail
 if not os.path.isdir(downloadsDir):
