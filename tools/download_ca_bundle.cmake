b644e23216a6d86a636f9b6d035b382829d7b81b
Dmitry Kazakov <dimula73@gmail.com>
2026-04-15 13:47:17 +0200

    [win] Use a custom CA bundle on Windows
    The certificates bundled in the Windows image
    are too old and it is difficult to update them for
    systadmins, so we have to fallback to uploading
    a copy of Mozilla certificates.

---
+++ tools/download_ca_bundle.cmake
@@ -0,0 +1,19 @@
+if (NOT KDECI_CA_BUNDLE_ARG)
+    message(FATAL_ERROR "KDECI_CA_BUNDLE_ARG argument is not set!")
+endif()
+
+if(DEFINED ENV{CMAKE_TLS_CAINFO} AND EXISTS "$ENV{CMAKE_TLS_CAINFO}")
+    message(WARNING "CMAKE_TLS_CAINFO environment is already set... weird, but we will continue")
+endif()
+
+if(DEFINED ENV{SSL_CERT_FILE} AND EXISTS "$ENV{SSL_CERT_FILE}")
+    message(WARNING "SSL_CERT_FILE environment is already set... weird, but we will continue")
+endif()
+
+message(STATUS "Downloading Mozilla CA certificate bundle to ${KDECI_CA_BUNDLE_ARG}...")
+file(DOWNLOAD
+    "https://curl.se/ca/cacert-2026-03-19.pem"
+    "${KDECI_CA_BUNDLE_ARG}"
+    TLS_VERIFY ON
+    EXPECTED_HASH SHA256=b6e66569cc3d438dd5abe514d0df50005d570bfc96c14dca8f768d020cb96171
+)
